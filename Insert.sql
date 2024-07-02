efef06c53f7da8dcffc0e5a7b93cec697182db40
Nithinbr95 <nithin.xworkz@gmail.com>
2024-07-03 00:40:24 +0530

    Add files via upload

---
+++ Insert.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE Employee;
+USE Employee;
+CREATE TABLE Employee(id int,emp_name varchar(20),emp_id int,emp_dept varchar(20),emp_sal bigint,emp_mng varchar(20),emp_mng_name varchar(20),mng_id int,emp_exp int)
+SELECT * FROM Employee;
+insert Employee values(1,'Shivu',201,'Sales',30000,'HR','Sunny',301,2);
+insert Employee values(2,'Karthik', 202, 'Marketing', 60000.00, 'Mark spencer', 'Mark Spencer', 302, 6);
+INSERT Employee VALUES(3,'Sagar', 203, 'HR', 50000.00, 'Jane Smith', 'Jane Smith', 301, 3);
+INSERT Employee VALUES(4,'Nithin', 205, 'Finance', 75000.00, 'Nancy Blue', 'Nancy Blue', 304, 10);
+INSERT Employee VALUES(5,'Sairaj', 208, 'HR', 48000.00, 'Jane Smith', 'Jane Smith', 301, 2);
+INSERT Employee VALUES(6,'Santosh', 210, 'Finance', 76000.00, 'Nancy Blue', 'Nancy Blue', 304, 11);
+INSERT Employee VALUES(7,'Raju', 213, 'HR', 49000.00, 'Jane Smith', 'Jane Smith', 301, 3);
+
+INSERT Employee VALUES(8,'Aaradhya', 214, 'IT', 71000.00, 'Michael Green', 'Michael Green', 303, 8);
+INSERT Employee VALUES(9,'Abdul', 217, 'Marketing', 63000.00, 'Mark Spencer', 'Mark Spencer', 302, 7);
+INSERT Employee VALUES(10,'Khushi', 212, 'Marketing', 62000.00, 'Mark Spencer', 'Mark Spencer', 302, 6);
+INSERT Employee VALUES(11,'Kavana', 207, 'Marketing', 61000.00, 'Mark Spencer', 'Mark Spencer', 302, 7);
+
+INSERT Employee VALUES(12,'Sanjay', 220, 'Finance', 78000.00, 'Nancy Blue', 'Nancy Blue',305,6);
+INSERT Employee VALUES(13,'Vishwa', 219, 'IT', 69000.00, 'Michael Green', 'Michael Green', 303, 10);
+INSERT Employee VALUES(14,'Ram', 218, 'HR', 47000.00, 'Jane Smith', 'Jane Smith', 301, 2);
+INSERT Employee VALUES(15,'Raghu', 220, 'HR', 47000.00, 'Jane Smith', 'Jane Smith', 301, 2);
+INSERT Employee VALUES(16,'gaja', 221, 'Developer', 47000.00, 'Don Smith', 'Don Smith', 303, 2);
+INSERT Employee VALUES(17,'Sanket', 222, 'Intern', 47000.00, 'John', 'John', 304, 7);
+INSERT Employee VALUES(18,'Hemanth', 223, 'Senior', 47000.00, 'Lufer', 'Lufer', 308, 8);
+INSERT Employee VALUES(19,'Prajwal', 224, 'consultant', 47000.00, 'Rocky', 'Rocky', 307, 9);
+
+
+create database Product;
+use product;
+
+ select * from product;
+insert product values(1,'tablet',200,2024,2025,'paracetamal','good'); 
+
+insert product values(2,'biscuit',20,2021,2022,'Good day','fine'); 
+insert product values(3,'juice',200,2023,2025,'Mirinda','good'); 
+insert product values(4,'chocalate',250,2020,2024,'Dairymilk','verygood'); 
+insert product values(5,'rice',2000,2019,2024,'Bullet','good'); 
+insert product values(6,'rava',200,2021,2024,'Gold','fine'); 
+insert product values(7,'sweet',350,2024,2024,'mysorepak','good'); 
+insert product values(8,'soap',20,2024,2026,'Rin','fine'); 
+insert product values(9,'shampoo',450,2020,2025,'Dove','good'); 
+insert product values(10,'waterbottel',30,2019,2022,'Bisleri','vey good'); 
+insert product values(11,'wheat',275,2024,2025,'Godwin','ok'); 
+insert product values(12,'Oil',150,2023,2025,'Goldwinner','good'); 
+insert product values(13,'paste',20,2022,2024,'colgate','fine'); 
+insert product values(14,'facewash',500,2000,2020,'Charcoal','bad'); 
+insert product values(15,'perfume',400,2018,2020,'Fog','worst'); 
+insert product values(16,'tablet',200,2024,2025,'','good'); 
+insert product values(17,'Milkshake',200,2024,2025,'Horlicks','good'); 
+insert product values(18,'salt',20,2020,2021,'Tata','expires'); 
+insert product values(19,'coconutoil',40,2024,2025,'parachute','fine'); 
+insert product values(20,'babysoap',250,2024,2025,'Johnson','good'); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
